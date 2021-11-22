ea6d1c966d09ec11b021cc558dba6b29df43857d
mohammadhssn <mohammadhssnalizadeh78@gmail.com>
2021-11-22 23:10:53 +0330

    create app core & custom user & connect to postgres

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,40 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
+
+
+class TestModel(TestCase):
+
+    def test_create_user_with_email_success(self):
+        """Test creating a new user with an email successfully"""
+        email = 'test@email.com'
+        password = 'testpassword'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test th email for a new user is normalized"""
+        email = 'test@EMAIL.COM'
+        user = get_user_model().objects.create_user(email=email, password='pass123')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValidationError):
+            get_user_model().objects.create_user(None, 'pass123')
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@email.com',
+            'pass123'
+        )
+
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
